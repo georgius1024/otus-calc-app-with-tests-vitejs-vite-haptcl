0bc97f447ebc9270430c7467a4bd7508e743c75e
Yurii Timofeev <georgius@ural.ru>
2024-12-05 11:18:10 +0100

    Added config option so Vitest will bypass Plawright tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,14 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/e2e/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+    ],
   },
   root: '.',
 });
